4c1373f0062be615059b1fa28a3449e99b858154
Sinead Frawley <sinead.frawley@gmail.com>
2019-03-17 16:57:40 +0000

    tided up datetime.py

--- datetime.py
+++ datetime.py
@@ -8,11 +8,15 @@ Question 8
 """
 
 import datetime
-my_date = datetime.datetime.now()
-dayOfTheWeek=my_date.strftime("%A")
-monthOftheYear=my_date.strftime("%B")
-dateOfMonth=my_date.strftime("%d")
-year=my_date.strftime("%Y")
-currentTime=my_date.strftime("%I:%M:%S %p")
-print(dayOfTheWeek + ', '+ monthOftheYear + ' '+dateOfMonth+'th '  + year + ' at ' + currentTime)
+
+def getDateInWordFormat():
+    my_date = datetime.datetime.now()
+    dayOfTheWeek=my_date.strftime("%A")
+    monthOftheYear=my_date.strftime("%B")
+    dateOfMonth=my_date.strftime("%d")
+    year=my_date.strftime("%Y")
+    currentTime=my_date.strftime("%I:%M:%S %p")
+    print(dayOfTheWeek + ', '+ monthOftheYear + ' '+dateOfMonth+'th '  + year + ' at ' + currentTime)
 '''Monday, January 10th 2019 at 1:15pm'''
+
+getDateInWordFormat()    
