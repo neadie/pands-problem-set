c32db86a061bf7d64deb1139d6dfc473929fb30e
Sinead Frawley <sinead.frawley@gmail.com>
2019-03-17 16:44:07 +0000

    added Enternumber.py so I wont reused code

---
+++ Enternumber.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 17 16:32:23 2019
+
+@author: SineadF
+"""
+
+def enterint():
+    numberImput = input("Please enter a positive number:")
+    try:
+        integerINput= int(numberImput)
+        print("Yes input string is an Integer.")
+        print("Input number value is: ", integerINput)
+        return integerINput
+    except ValueError:
+        print("That's not an int!")
+        print("No.. input string is not an Integer. It's a string")
