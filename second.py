196a9abc0ae255dfc8749b614577542a2a149bf2
Sinead Frawley <sinead.frawley@gmail.com>
2019-03-30 20:34:28 +0000

    add commits to code

--- second.py
+++ second.py
@@ -10,7 +10,12 @@ Created on Sun Feb  3 22:41:56 2019  reference - https://www.afterhoursprogrammi
 
 ''' 
 ''' Change working directory in python '''
+  
 
+''' To call this file is second.py " what ever file name you what to call if in same direcory just file name if in another 
+ directory path of file is required too
+ 
+ '''
 import sys
 
 def everySecondLine(fileName):
